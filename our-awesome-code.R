796b1b6f1aa99bb33b7e9203635f1276115ed92b
BenIsaacs <ben.isaacs@mq.edu.au>
2017-12-13 15:40:25 +1100

    Created my function

--- our-awesome-code.R
+++ our-awesome-code.R
@@ -1,5 +1,13 @@
-awesome_function_1 <- function(argument1) {
-    _______
+awesome_function_1 <- function(argument1, argument2) {
+    if ((argument1 - argument2) > 0) {
+  return(c(argument1, "winner"))
+    }
+else if ((argument1 - argument2) < 0) {
+  return(c(argument2, "winner"))
+}
+  else {
+    return ("draw")
+  }
 }
 
 awesome_function_2 <- function(argument1) {
